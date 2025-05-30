3c04fb9018d5d923253206881614d778131c7676
jpage <james.page@ced.com>
2025-05-29 22:57:21 -0500

    update: scandit 7.3.0

--- app/screens/SimpleScandit.tsx
+++ app/screens/SimpleScandit.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react';
-import { AppState, AppStateStatus, BackHandler, Platform, Pressable, StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useMemo, useRef, useState } from 'react';
+import { BackHandler, Platform, Pressable } from 'react-native';
 import { AMS_365_SCANDIT_LICENSE_KEY, AMS_MOBILE_SCANDIT_LICENSE_KEY, MOBILE_ENV } from '@env';
-import { StackActions, useIsFocused, useNavigation } from '@react-navigation/native';
-import type { StackNavigationProp } from '@react-navigation/stack';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
 import {
-  Barcode,
   BarcodeCapture,
+  BarcodeCaptureListener,
   BarcodeCaptureOverlay,
   BarcodeCaptureOverlayStyle,
   BarcodeCaptureSession,
@@ -19,54 +19,51 @@ import {
   DataCaptureContext,
   DataCaptureView,
   FrameSourceState,
-  LaserlineViewfinder,
-  LaserlineViewfinderStyle,
+  FrameData,
   RectangularViewfinder,
   RectangularViewfinderStyle,
   RectangularViewfinderLineStyle,
-  VideoResolution
+  VideoResolution,
+  TorchState
 } from 'scandit-react-native-datacapture-core';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { useScanditStore } from '@app/stores';
 import { requestCameraPermissionsIfNeeded } from '@app/utils';
 import type { ScanditStore, StackParamsList } from '@app/types';
 
-export default function SimpleScan(): JSX.Element {
-  const isFocused = useIsFocused();
+export default function SimpleScandit() {
+  //================================ variables ================================
+  const navigation: any = useNavigation<StackNavigationProp<StackParamsList>>();
   const viewRef = useRef<DataCaptureView>(null);
+  const camera = useRef<Camera | null>(null);
+  const barcodeCaptureMode = useRef<BarcodeCapture | null>(null);
+  // Due to a React Native issue with firing the AppState 'change' event on iOS, we want to avoid triggering
+  // a startCapture/stopCapture on the scanner twice in a row. We work around this by keeping track of the
+  // latest command that was run, and skipping a repeated call for starting or stopping scanning.
+  const setupFlagRef = useRef(false);
+  const lastCommand = useRef<string | null>(null);
+  const { simpleScanditResults, setSimpleScanditResults } = useScanditStore((store: ScanditStore) => store);
+  const [isCameraTorchAvailable, setIsCameraTorchAvailable] = useState(false);
+  const [isTorchOn, setIsTorchOn] = useState(false);
+  const [localScanResults, setLocalScanResults] = useState('');
   const dataCaptureContext = useMemo(() => {
+    // Enter your Scandit License key here.
+    // Your Scandit License key is available via your Scandit SDK web account.
     return DataCaptureContext.forLicenseKey(
       MOBILE_ENV === 'prod' && Platform.OS === 'android' ? AMS_365_SCANDIT_LICENSE_KEY : AMS_MOBILE_SCANDIT_LICENSE_KEY
     );
   }, []);
-  const navigation: any = useNavigation<StackNavigationProp<StackParamsList>>();
-  const { simpleScanditResults, setSimpleScanditResults } = useScanditStore((store: ScanditStore) => store);
-  const [camera, setCamera] = useState<Camera | null>(Camera.default);
-  const [barcodeCaptureMode, setBarcodeCaptureMode] = useState<BarcodeCapture | null>(null);
-  const [isBarcodeCaptureEnabled, setIsBarcodeCaptureEnabled] = useState(false);
-  const [cameraState, setCameraState] = useState<FrameSourceState | null>(null);
 
-  // Due to a React Native issue with firing the AppState 'change' event on iOS, we want to avoid triggering
-  // a startCapture/stopCapture on the scanner twice in a row. We work around this by keeping track of the
-  // latest command that was run, and skipping a repeated call for starting or stopping scanning.
-  const lastCommand = useRef<string | null>(null);
-
-  const handleAppStateChange = (nextAppState: AppStateStatus) => {
-    if (nextAppState.match(/inactive|background/)) {
-      stopCapture();
-    } else {
-      startCapture();
-    }
-  };
-
+  //================================ functions ================================
   const setupScanning = () => {
     // Use the world-facing (back) camera and set it as the frame source of the context. The camera is off by
     // default and must be turned on to start streaming frames to the data capture context for recognition.
     const cameraSettings = new CameraSettings();
     cameraSettings.preferredResolution = VideoResolution.FullHD;
-    camera?.applySettings(cameraSettings);
 
-    dataCaptureContext.setFrameSource(camera);
-    setCamera(camera);
+    const cameraInstance = Camera.withSettings(cameraSettings);
+    dataCaptureContext.setFrameSource(cameraInstance);
+    camera.current = cameraInstance;
 
     // The barcode capturing process is configured through barcode capture settings
     // and are then applied to the barcode capture instance that manages barcode recognition.
@@ -97,20 +94,52 @@ export default function SimpleScan(): JSX.Element {
     // Create new barcode capture mode with the settings from above.
     const barcodeCapture = BarcodeCapture.forContext(dataCaptureContext, settings);
 
+    // By default, every time a barcode is scanned, a sound (if not in silent mode) and a vibration are played.
+    // Uncomment the following lines to set a success feedback without sound and vibration.
+    // const feedback = BarcodeCaptureFeedback.default;
+    // feedback.success = new Feedback(null, null);
+    // barcodeCapture.feedback = feedback;
+    // Uncomment the following line to set a success feedback without sound and vibration.
+    // const defaultFeedback = Feedback.defaultFeedback;
+
     // Register a listener to get informed whenever a new barcode got recognized.
-    const barcodeCaptureListener = {
-      didScan: (_: BarcodeCapture, session: BarcodeCaptureSession) => {
-        const barcode: Barcode | null = session.newlyRecognizedBarcode;
-        if (barcode === null) {
-          const symbology = new SymbologyDescription(Symbology.QR);
-          navigation.goBack();
-          return;
-        }
-        // prevent random scans from happening, and only scan the barcode
+    const barcodeCaptureListener: BarcodeCaptureListener = {
+      didScan: async (
+        barcodeCapture: BarcodeCapture,
+        session: BarcodeCaptureSession,
+        _getFrameData: () => Promise<FrameData>
+      ) => {
+        const barcode = session.newlyRecognizedBarcode;
+        if (barcode == null) return;
+
         const symbology = new SymbologyDescription(barcode.symbology);
 
-        // add scan results to the store
-        setSimpleScanditResults(`${barcode.data}`);
+        // The `alert` call blocks execution until it's dismissed by the user. As no further frames would be processed
+        // until the alert dialog is dismissed, we're showing the alert through a timeout and disabling the barcode
+        // capture mode until the dialog is dismissed, as you should not block the BarcodeCaptureListener callbacks for
+        // longer periods of time. See the documentation to learn more about this.
+        barcodeCapture.isEnabled = false;
+
+        // Use the following code to reject barcodes.
+        // By uncommenting the following lines, barcodes not starting with 09: are ignored.
+        // if (!barcode.data?.startsWith('09:')) {
+        //    // We temporarily change the brush, used to highlight recognized barcodes, to a transparent brush.
+        //   overlay.brush = Brush.transparent;
+        //   return;
+        // }
+        // Otherwise, if the barcode is of interest, we want to use a brush to highlight it.
+        // overlay.brush = new Brush(
+        //   Color.fromHex('FFF0'),
+        //   Color.fromHex('FFFF'),
+        //   3
+        // );
+
+        // We also want to emit a feedback (vibration and, if enabled, sound).
+        // By default, every time a barcode is scanned, a sound (if not in silent mode) and a vibration are played.
+        // To emit a feedback only when necessary, it is necessary to set a success feedback without sound and
+        // vibration when setting up Barcode Capture (in this case in the `setupScanning`).
+        // defaultFeedback.emit();
+        setLocalScanResults(`${barcode.data}`);
       }
     };
 
@@ -124,119 +153,163 @@ export default function SimpleScan(): JSX.Element {
       null,
       BarcodeCaptureOverlayStyle.Frame
     );
-    overlay.viewfinder = new LaserlineViewfinder(LaserlineViewfinderStyle.Animated);
-    /* or
-    overlay.viewfinder = new RectangularViewfinder(RectangularViewfinderStyle.Legacy);
-    overlay.viewfinder = new RectangularViewfinder(RectangularViewfinderStyle.Square);
-    overlay.viewfinder = new RectangularViewfinder(RectangularViewfinderStyle.Light);
-    */
+    overlay.viewfinder = new RectangularViewfinder(
+      RectangularViewfinderStyle.Square,
+      RectangularViewfinderLineStyle.Light
+    );
     viewRef.current?.addOverlay(overlay);
-    setBarcodeCaptureMode(barcodeCapture);
+    barcodeCaptureMode.current = barcodeCapture;
   };
-
+  //---------------------------------------------------------------------------
+  const setupCameraBottomButtons = async (): Promise<void> => {
+    if (!camera.current) return;
+    const isTorchAvailable = await camera.current.getIsTorchAvailable();
+    setIsCameraTorchAvailable(isTorchAvailable);
+  };
+  //---------------------------------------------------------------------------
   const startCapture = async () => {
+    console.log('begin startCapture()');
     if (lastCommand.current === 'startCapture') {
       return;
     }
     lastCommand.current = 'startCapture';
-    startCamera();
+    await startCamera();
+    if (barcodeCaptureMode.current) {
+      barcodeCaptureMode.current.isEnabled = true;
+    }
+    console.log('end startCapture()');
   };
-
-  const stopCapture = () => {
+  //---------------------------------------------------------------------------
+  const stopCapture = async () => {
+    console.log('begin stopCapture()');
     if (lastCommand.current === 'stopCapture') {
       return;
     }
     lastCommand.current = 'stopCapture';
-    stopCamera();
+    if (barcodeCaptureMode.current) {
+      barcodeCaptureMode.current.isEnabled = false;
+    }
+    await stopCamera();
+    console.log('end stopCapture()');
   };
-
-  const startCamera = () => {
-    // Switch camera on to start streaming frames and enable the barcode capture mode.
-    // The camera is started asynchronously and will take some time to completely turn on.
-    requestCameraPermissionsIfNeeded()
-      .then(() => setCameraState(FrameSourceState.On))
-      .catch(() => BackHandler.exitApp());
-  };
-
-  const stopCamera = () => {
-    if (camera) {
-      setCameraState(FrameSourceState.Off);
+  //---------------------------------------------------------------------------
+  const startCamera = async () => {
+    console.log('begin startCamera()');
+    try {
+      // Switch camera on to start streaming frames and enable the barcode capture mode.
+      // The camera is started asynchronously and will take some time to completely turn on.
+      await requestCameraPermissionsIfNeeded();
+      if (camera.current) {
+        await camera.current.switchToDesiredState(FrameSourceState.On);
+        const cameraCurrentState = await camera.current.getCurrentState();
+        if (cameraCurrentState === FrameSourceState.On) {
+          // checks what options the camera has and sets up the buttons
+          await setupCameraBottomButtons();
+        }
+      }
+      console.log('end startCamera()');
+    } catch (error: any) {
+      BackHandler.exitApp();
     }
   };
-
-  const handleCancel = () => {
-    navigation.goBack();
+  //---------------------------------------------------------------------------
+  const stopCamera = async () => {
+    console.log('begin stopCamera()');
+    try {
+      if (camera.current) {
+        // turn off camera torch state if it's on
+        if (isCameraTorchAvailable && camera.current.desiredTorchState !== TorchState.Off) {
+          camera.current.desiredTorchState = TorchState.Off;
+        }
+        // then, turn off camera
+        await camera.current.switchToDesiredState(FrameSourceState.Off);
+        const cameraCurrentState = await camera.current.getCurrentState();
+        // then, go back to previous screen
+        console.log('cameraCurrentState:', cameraCurrentState);
+      }
+      console.log('end stopCamera()');
+    } catch (error: any) {
+      BackHandler.exitApp();
+    }
   };
-
-  useEffect(() => {
-    const handleAppStateChangeSubscription = AppState.addEventListener('change', handleAppStateChange);
-    setupScanning();
-    startCapture();
-    const backHandler = BackHandler.addEventListener('hardwareBackPress', () => {
-      handleCancel();
-      return true;
-    });
-    return () => {
-      handleAppStateChangeSubscription.remove();
-      stopCapture();
-      dataCaptureContext.dispose();
-      backHandler.remove();
-    };
+  //---------------------------------------------------------------------------
+  const toggleTorch = () => {
+    if (!camera.current) return;
+    camera.current.desiredTorchState = isTorchOn ? TorchState.Off : TorchState.On;
+    setIsTorchOn(!isTorchOn);
+  };
+  //---------------------------------------------------------------------------
+  const initSetup = useCallback(async () => {
+    if (!setupFlagRef.current) {
+      setupScanning();
+      await startCapture();
+      setupFlagRef.current = true;
+    }
   }, []);
 
-  useEffect(() => {
-    if (camera) {
-      if (cameraState === null) {
-        camera.switchToDesiredState(FrameSourceState.Off);
-      } else {
-        camera.switchToDesiredState(cameraState);
+  //================================== setup ==================================
+  useFocusEffect(
+    useCallback(() => {
+      if (!!simpleScanditResults) {
+        navigation.goBack();
+        return;
       }
-    }
-    if (cameraState === FrameSourceState.On) {
-      setIsBarcodeCaptureEnabled(true);
-    } else if (cameraState === FrameSourceState.Off) {
-      setIsBarcodeCaptureEnabled(false);
-    }
-  }, [cameraState]);
+      initSetup();
+      const backHandler = BackHandler.addEventListener('hardwareBackPress', () => {
+        navigation.goBack();
+        return true;
+      });
+      return async () => {
+        backHandler.remove();
+        await stopCapture();
+        dataCaptureContext.removeAllModes();
+      };
+    }, [simpleScanditResults])
+  );
+  useFocusEffect(
+    useCallback(() => {
+      if (!!localScanResults) setSimpleScanditResults(localScanResults);
+    }, [localScanResults])
+  );
 
-  useEffect(() => {
-    if (barcodeCaptureMode) {
-      barcodeCaptureMode.isEnabled = isBarcodeCaptureEnabled;
-    }
-    // handle screen return
-    if (!isFocused || (isFocused && !simpleScanditResults)) return;
-    if (navigation.canGoBack()) {
-      navigation.goBack();
-    }
-  }, [isBarcodeCaptureEnabled]);
-
-  useEffect(() => {
-    // after updating scan results
-    // stop camera and get ready to return to previous screen
-    if (isFocused && !!simpleScanditResults) {
-      stopCapture();
-    }
-  }, [simpleScanditResults]);
-
+  //================================== render =================================
   return (
-    <View style={StyleSheet.absoluteFill}>
+    <>
       <Pressable
         style={{
           position: 'absolute',
-          top: 10,
-          right: 10,
+          top: 30,
+          left: 0,
           paddingHorizontal: 16,
           paddingVertical: 8,
-          zIndex: 10,
-          borderRadius: 4,
-          borderColor: '#ffffff',
-          borderWidth: 1
+          zIndex: 10
         }}
-        onPress={handleCancel}
+        onPress={() => navigation.goBack()}
       >
-        <Text style={{ color: '#ffffff' }}>Cancel</Text>
+        <MaterialCommunityIcons name="keyboard-backspace" size={24} color="#ffffff" />
       </Pressable>
       <DataCaptureView style={{ flex: 1 }} context={dataCaptureContext} ref={viewRef} />
-    </View>
+      {isCameraTorchAvailable ? (
+        <Pressable
+          style={{
+            position: 'absolute',
+            bottom: 20,
+            right: 20,
+            padding: 16,
+            zIndex: 10,
+            borderRadius: 8,
+            backgroundColor: isTorchOn ? '#ffffff' : 'rgba(0, 0, 0, 0.7)',
+            borderWidth: 1
+          }}
+          onPress={toggleTorch}
+        >
+          <MaterialCommunityIcons
+            name={isTorchOn ? 'flashlight' : 'flashlight-off'}
+            size={16}
+            color={isTorchOn ? '000000' : '#ffffff'}
+          />
+        </Pressable>
+      ) : null}
+    </>
   );
 }
